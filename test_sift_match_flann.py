c6de4a8c5ac21792d34f2878c91ec415fdc84823
openvisual <sunabove@nate.com>
2020-09-15 13:52:51 +0900

    **

--- test_sift_match_flann.py
+++ test_sift_match_flann.py
@@ -21,31 +21,59 @@ kp1, des1 = sift.detectAndCompute(img1,None)
 kp2, des2 = sift.detectAndCompute(img2,None)
 
 # FLANN parameters
-FLANN_INDEX_KDTREE = 0
+FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 search_params = dict(checks=50)   # or pass empty dictionary
 
 flann = cv2.FlannBasedMatcher(index_params,search_params)
 
+#Ratio test
 matches = flann.knnMatch(des1, des2, k=2)
 
-# Need to draw only good matches, so create a mask
-matchesMask = [[0, 0] for i in range(len(matches))]
+dim = max( [ len(img1), len(img1[0])] )/5
+dim = dim*dim
 
-# ratio test as per Lowe's paper
-for i, (m, n) in enumerate(matches):
-    if m.distance < 0.7*n.distance:
-        matchesMask[i] = [1, 0]
+goods = []
+matchesMask = []
+
+for i, (m1, m2) in enumerate(matches):
+    valid = True
+
+    if valid and ( m1.distance > 0.7 * m2.distance ):
+        valid = False
+    pass
+
+    if valid :
+        pt1 = kp1[m1.queryIdx].pt
+        pt2 = kp2[m1.trainIdx].pt
+
+        dx = pt1[0] - pt2[0]
+        dy = pt1[1] - pt2[1]
+
+        if dx*dx + dy*dy > dim :
+            valid = False
+        pass
+
+        if valid :
+            goods.append( [m1, m2] )
+            matchesMask.append( [1,0] )
+            ## Notice: How to get the index
+            print(i, pt1, pt2 )
+            if i % 5 == 0:
+                ## Draw pairs in purple, to make sure the result is ok
+                cv2.circle(img1, (int(pt1[0]),int(pt1[1])), 5, (255,0,255), -1)
+                cv2.circle(img2, (int(pt2[0]),int(pt2[1])), 5, (255,0,255), -1)
+            pass
         pass
     pass
 pass
 
-draw_params = dict(matchColor = (0, 255, 0),
-                   singlePointColor = (255, 0, 0),
+draw_params = dict(matchColor = (255, 0, 0),
+                   singlePointColor = (0, 0, 255),
                    matchesMask = matchesMask,
                    flags = 0)
 
-img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, matches, None, **draw_params)
+img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, goods, None, **draw_params)
 
 plt.imshow(img3)
 plt.show()
