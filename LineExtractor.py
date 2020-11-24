5b9a43f7bca19094bf2efd5f5f40b9a098c0351f
openvisual <67694546+openvisual@users.noreply.github.com>
2020-11-24 19:19:35 +0900

    **

--- LineExtractor.py
+++ LineExtractor.py
@@ -87,6 +87,7 @@ class LineExtractor ( Common ):
         channel_cnt = img_org.shape[2]
 
         margin_ratio = 5
+        margin_ratio = 0
 
         self.width  = width
         self.height = height
@@ -246,16 +247,17 @@ class LineExtractor ( Common ):
 
         useContour = True
         if useContour:
+            lineWidth = 1
             contours = curr_image.extract_contours()
-            contours_image = curr_image.draw_contours(contours, lineWidth=2)
+            contours_image = curr_image.draw_contours(contours, lineWidth=lineWidth)
             contours_image.save_img_as_file(img_path, contours_image.algorithm)
 
             contours = contours_image.filter_contours(contours)
-            contours_image = contours_image.draw_contours(contours, lineWidth=2)
+            contours_image = contours_image.draw_contours(contours, lineWidth=lineWidth)
             contours_image.save_img_as_file(img_path, contours_image.algorithm + "_filtered")
 
             lines = contours_image.filter_lines_only( contours )
-            contours_image = contours_image.draw_contours(lines, lineWidth=2)
+            contours_image = contours_image.draw_contours(lines, lineWidth=lineWidth)
             contours_image.save_img_as_file(img_path, contours_image.algorithm + "_lines_only")
 
             curr_image = contours_image
@@ -315,8 +317,10 @@ if __name__ == '__main__':
     files = []
 
     img_path = "./data_yegan/set_01/_1018843.JPG"
+    img_path = "./data_yegan/set_06/data4.JPG"
     #img_path = "./data_yegan/set_04/P1010015.JPG"
-    #img_path = "./data_yegan/set_04"
+    img_path = "./data_yegan/set_04"
+    img_path = "./data_yegan/set_05"
 
     #img_path = "./data_yegan/set_01"
 
